02ad0eefb17d782b54af4067fca0c32b78eb9086
liuxun <1193127296@qq.com>
2022-05-18 07:12:12 +0800

    6 硬盘读写

--- boot.asm
+++ boot.asm
@@ -2,24 +2,79 @@
 mov ax,3
 int 0x10
 xchg bx,bx
-call print
+mov di,0x1000
+mov ecx,0
+mov bl,1
+read_disK:
+  
+  mov dx, 0x1f2;设置扇区数量
+  mov al,bl
+  out dx,al
 
-jmp $
-print:
-  mov ah,0x0e
-  mov si,booting
-  call .next
-.next:
-  mov al, [si]
-  cmp al,0
-  jz .done
-  int 0x10
-  inc si
-  jmp .next
-.done:
+  inc dx ;0x1f3
+  mov al,cl
+  out dx,al
+
+  shr ecx,8
+  inc dx ;0x1f4
+  mov al,cl
+  out dx,al
+
+  shr ecx,8
+  inc dx ;0x1f5
+  mov al,cl
+  out dx,al
+  
+  shr ecx,8
+  inc dx ;0x1f6
+  and cl,0b1111
+  or cl,0b1110_0000
+  mov al,cl
+  out dx,al
+  
+  inc dx ;0x1f7 读硬盘
+  mov al,0x20
+  out dx,al
+
+  xor ecx,ecx
+  mov cl,bl
+
+  call .read
+
+  xchg bx,bx
+  jmp $
+  
+.read:
+  push cx
+  call .waits
+  call .reads
+  loop .read
+  pop cx
+.waits:
+  mov dx,0x1f7
+  .check:
+    in al,dx
+    jmp $+2
+    jmp $+2
+    jmp $+2
+    and al,0b1000_1000
+    cmp al,0b0000_1000
+    jnz .check
   ret
-booting:
-  db "booting Olinux....",10,13,0
+.reads:
+  mov dx, 0x1f0
+  mov cx, 256
+  .readw:
+    in ax, dx
+    jmp $+2
+    jmp $+2
+    jmp $+2
+    mov [edi],ax
+    add edi,2
+    loop .readw
+  ret
+
+
 times 510 - ($ - $$) db 4
 
 db 0x55, 0xaa
